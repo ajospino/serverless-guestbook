f4cadcb76933f787e248f0c17988202596b73aa6
aospino1 <aospino@playvox.com>
2022-07-15 10:15:08 -0500

    Change

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://2e9fc1db.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
